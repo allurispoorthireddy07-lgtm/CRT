eb3e0013ee49ca8de365a60971a5c49b83c1d8ab
spoorthi reddy <allurispoorthireddy07@gmail.com>
2026-03-11 09:26:34 +0530

    :wq

---
+++ IV-SEM/Python/Practice/M03_core_concepts/S02/PS02_python_collections.py
@@ -0,0 +1,17 @@
+#Accessing of list:
+a=[10,20,30,40,50]
+print(a[0])
+print(a[-1])
+
+# Removing Elements from list:
+a=[10,20,30,40,50]
+a.remove(40)
+print(a)
+a.pop()
+print(a)
+
+# Slicing
+a=[10,20,30,40,50]
+print(a[0:])
+print(a[2:])
+#reverse the list
